f2783678f97ffe4fd56b2787077465461e5d4e17
choco <superbeaver.180809@gmail.com>
2022-10-19 20:02:13 +0900

    webpack経由でvue3が動くようにした

---
+++ app/javascript/hello.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>{{message}}</div>
+</template>
+
+<script>
+export default {
+  name: "hello",
+  data() {
+    return {
+      message: "Hello Vue!"
+    }
+  }
+}
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path    = require("path")
+const webpack = require("webpack")
+const { VueLoaderPlugin } = require("vue-loader")
+
+module.exports = {
+  mode: "production",
+  devtool: "source-map",
+  entry: {
+    application: "./app/javascript/application.js"
+  },
+  output: {
+    filename: "[name].js",
+    sourceMapFilename: "[file].map",
+    path: path.resolve(__dirname, "app/assets/builds"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader"
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1
+    }),
+    new VueLoaderPlugin()
+  ]
+}
